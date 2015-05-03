532cfea824ff86ce717abb9341757228db1c2ff2
negoxv <negoxv@gmail.com>
2015-05-04 00:55:24 +0200

    version quiz7 dbrecien creada

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,16 @@
 //get /quizes/question
 exports.question=function(req, res){
-	res.render('quizes/question', {pregunta: 'capital de Italia'});
+	models.Quiz.findAll().succes(function(quiz){
+	res.render('quizes/question', {pregunta: pregunta: quiz[0].pregunta});
+})
 };
 exports.answer=function(req, res){
-	if(req.query.respuesta === 'Roma'){
+	models.Quiz.findAll().succes(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	}else{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
+	})
+	
 };
