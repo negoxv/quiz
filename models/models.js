53979bf2fde7b3326c640adbac6752542b8dc9e0
negoxv <negoxv@gmail.com>
2015-05-04 14:10:45 +0200

    version8 db

--- models/models.js
+++ models/models.js
@@ -23,14 +23,14 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     omitNull: true      // solo Postgres
   }     
 );
-
-var Quiz =sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path=path.join(__dirname,'quiz');
+var Quiz =sequelize.import(quiz_path);
 exports.Quiz=Quiz;
 sequelize.sync().success(function(){
 	Quiz.count().success(function(count){
 		if(count===0){
-			Quiz.create({ pregunta:'Capital de Italia'
-							respuesta:'Roma'
+			Quiz.create({ pregunta:'Capital de Italia',
+						respuesta:'Roma'
 
 		})
 			.success(function(){console.log('Base de datos inicializada')});
