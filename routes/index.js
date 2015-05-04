aab82342fe83abcef2295d0fbb7459f772c7b2aa
negoxv <negoxv@gmail.com>
2015-05-04 18:18:22 +0200

    version db 9

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController =require('../controllers/quiz_controller');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
-});
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes',          quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 module.exports = router;
