7abd5973ec903b65af4d1a322a5520c38c90ba43
negoxv <negoxv@gmail.com>
2015-05-05 00:01:40 +0200

    version definitva

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
+
 //get /quizes/question
 exports.question=function(req, res){
 	res.render('quizes/question', {pregunta: 'capital de Italia'});
-=======
-//get /quizes/questio
-var models = require('../models/models.js');
-exports.load=function(req, res, next, quizId){
-	models.Quiz.find(quizId).then(function(quiz){
-		if(quiz){
-			req.quiz = quiz;
-			next();
-		}else{next(new Error('No existe quizId=' + quizId));}
+};
 
-}).catch(function(error){next(error);});
-};
-exports.show=function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-	res.render('quizes/show', { quiz: quiz});
-}
-)
->>>>>>> origin/master
-};
+
 exports.answer=function(req, res){
 	if(req.query.respuesta === 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
