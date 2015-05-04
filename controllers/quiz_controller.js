ea0ca337e9017528c18926864ff39a91fac25203
negoxv <negoxv@gmail.com>
2015-05-04 14:19:37 +0200

    version8 db

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,12 @@
 //get /quizes/questio
 var models = require('../models/models.js');
 exports.question=function(req, res){
-	models.Quiz.findAll().succes(function(quiz){
+	models.Quiz.findAll().success(function(quiz){
 	res.render('quizes/question', {pregunta: quiz[0].pregunta});
 })
 };
 exports.answer=function(req, res){
-	models.Quiz.findAll().succes(function(quiz){
+	models.Quiz.findAll().success(function(quiz){
 		if(req.query.respuesta === quiz[0].respuesta){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	}else{
