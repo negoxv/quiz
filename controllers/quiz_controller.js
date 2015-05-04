641ff20f9bdfe8d27382e36acb3da61863d3b61d
negoxv <negoxv@gmail.com>
2015-05-04 18:41:41 +0200

    version db 9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,19 @@
 //get /quizes/questio
 var models = require('../models/models.js');
+exports.load=function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(function(quiz){
+		if(quiz){
+			req.quiz = quiz;
+			next();
+		}else{next(new Error('No existe quizId=' + quizId));}
+
+}).catch(function(error){next(error);});
+};
 exports.show=function(req, res){
-	models.Quiz.find.(req.params.quizId).then(function(quiz){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
 	res.render('quizes/show', {quiz: quiz});
-})
+}
+)
 };
 exports.answer=function(req, res){
 	models.Quiz.find(reqId).then(function(quiz){
@@ -18,6 +28,6 @@ exports.answer=function(req, res){
 exports.index=function(req, res){
 	models.Quiz.findAll().then(function(quizes){
 	res.render('quizes/index.ejs', {quizes: quizes});
-})
+}).catch(function(error){next(error);});
 };
-ex
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var quizController =require('../controllers/quiz_controller');
+router.get('/',function(res,req){
+	res.render('index', {title: 'Quiz'});
+});
+
+router.param('quizId', quizController.load);
 /* GET home page. */
 router.get('/quizes',          quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
