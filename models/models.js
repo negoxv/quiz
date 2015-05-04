b5a943424e795125cf8ddfee71bfa66a8822781b
negoxv <negoxv@gmail.com>
2015-05-04 16:47:04 +0200

    version funciona en local

--- models/models.js
+++ models/models.js
@@ -27,13 +27,13 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var Quiz =sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz=Quiz;
 sequelize.sync().then(function(){
-	Quiz.count().success(function(count){
+	Quiz.count().then(function(count){
 		if(count===0){
 			Quiz.create({ pregunta:'Capital de Italia',
 						respuesta:'Roma'
 
 		})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		}
 	
 	
