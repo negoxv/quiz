0f5a9f038aec3aec66d158aeaa389137b1c8bfe5
negoxv <negoxv@gmail.com>
2015-05-11 12:24:00 +0200

    version 11

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,28 @@
+var models = require('../models/models.js');
 
-//get /quizes/question
-exports.question=function(req, res){
-	res.render('quizes/question', {pregunta: 'capital de Italia'});
+// GET /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
+  })
 };
 
-
-exports.answer=function(req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', { quiz: quiz});
+  })
 };
-exports.creditos=function(req, res){
-	res.render('author', {creditos: 'creditos de marta y luis'});
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', 
+                 { quiz: quiz, respuesta: 'Incorrecto'});
+    }
+  })
 };
